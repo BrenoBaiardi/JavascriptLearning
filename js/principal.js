454d0d49c8ba2099690c04057788fe37423ef6e6
Breno César Baiardi <brenocbo@gmail.com>
2020-02-22 16:28:45 -0300

    fixing thing in CSS and making loop to update table

--- js/principal.js
+++ js/principal.js
@@ -1,33 +1,33 @@
 var titulo = document.querySelector(".titulo");
 titulo.textContent = "Aparecida Nutricionista";
 
-var paciente = document.querySelector("#primeiro-paciente");
+//var paciente = document.querySelector("#primeiro-paciente");
+var pacientes = document.querySelectorAll(".paciente");
+for (var i = 0; i < pacientes.length; i++) {
 
-var tdPeso = paciente.querySelector(".info-peso");
-var peso = tdPeso.textContent;
+  var tdPeso = pacientes[i].querySelector(".info-peso");
+  var tdAltura = pacientes[i].querySelector(".info-altura");
+  var tdImc = pacientes[i].querySelector(".info-imc");
+  var peso = tdPeso.textContent;
+  var altura = tdAltura.textContent;
 
-var tdAltura = paciente.querySelector(".info-altura");
-var altura = tdAltura.textContent;
+  var pesoEValido = true;
+  var palturaEValida = true;
+  var imc = peso / (altura * altura)
 
-var pesoEValido = true;
-var palturaEValida = true;
-var imc = peso / (altura * altura)
-var tdImc = paciente.querySelector(".info-imc");
+  if (peso <= 0 || peso >= 1000){
+    tdImc.textContent = "Peso Inválido";
+    pesoEValido = false;
+    pacientes[i].classList.add("paciente-invalido");
+  }
 
-if (peso <= 0 || peso >= 1000){
-  tdImc.textContent = "Peso Inválido";
-  pesoEValido = false;
-}
+  if (altura <= 0 || altura >= 2.5){
+    tdImc.textContent = "Altura Inválida";
+    palturaEValida = false;
+    pacientes[i].classList.add("paciente-invalido");
+  }
 
-if (altura <= 0 || altura >= 2.5){
-  tdImc.textContent = "Altura Inválida";
-  palturaEValida = false;
-}
-
-
-if (pesoEValido && palturaEValida){
-  tdImc.textContent = imc;
-}
-else{
-
+  if (pesoEValido && palturaEValida){
+    tdImc.textContent = imc.toFixed(2);
+  }
 }
