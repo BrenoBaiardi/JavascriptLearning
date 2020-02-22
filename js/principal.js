e4386d83651fba6d790bfb2c6029c136b61c281a
Breno César Baiardi <brenocbo@gmail.com>
2020-02-22 15:05:28 -0300

    manipulating td & tr values given conditions

--- js/principal.js
+++ js/principal.js
@@ -1,2 +1,33 @@
 var titulo = document.querySelector(".titulo");
 titulo.textContent = "Aparecida Nutricionista";
+
+var paciente = document.querySelector("#primeiro-paciente");
+
+var tdPeso = paciente.querySelector(".info-peso");
+var peso = tdPeso.textContent;
+
+var tdAltura = paciente.querySelector(".info-altura");
+var altura = tdAltura.textContent;
+
+var pesoEValido = true;
+var palturaEValida = true;
+var imc = peso / (altura * altura)
+var tdImc = paciente.querySelector(".info-imc");
+
+if (peso <= 0 || peso >= 1000){
+  tdImc.textContent = "Peso Inválido";
+  pesoEValido = false;
+}
+
+if (altura <= 0 || altura >= 2.5){
+  tdImc.textContent = "Altura Inválida";
+  palturaEValida = false;
+}
+
+
+if (pesoEValido && palturaEValida){
+  tdImc.textContent = imc;
+}
+else{
+
+}
