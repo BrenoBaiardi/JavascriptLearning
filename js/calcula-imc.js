1fa8f1dfdb72b8c60b9adc4ab2ce7cc1c287339c
Breno César Baiardi <brenocbo@gmail.com>
2020-02-25 12:53:29 -0300

    refactoring code

---
+++ js/calcula-imc.js
@@ -0,0 +1,40 @@
+var titulo = document.querySelector(".titulo");
+titulo.textContent = "Aparecida Nutricionista";
+
+//var paciente = document.querySelector("#primeiro-paciente");
+var pacientes = document.querySelectorAll(".paciente");
+for (var i = 0; i < pacientes.length; i++) {
+
+  var tdPeso = pacientes[i].querySelector(".info-peso");
+  var tdAltura = pacientes[i].querySelector(".info-altura");
+  var tdImc = pacientes[i].querySelector(".info-imc");
+  var peso = tdPeso.textContent;
+  var altura = tdAltura.textContent;
+
+  var pesoEValido = true;
+  var palturaEValida = true;
+  var imc = peso / (altura * altura)
+
+  if (peso <= 0 || peso >= 1000){
+    tdImc.textContent = "Peso Inválido";
+    pesoEValido = false;
+    pacientes[i].classList.add("paciente-invalido");
+  }
+
+  if (altura <= 0 || altura >= 2.5){
+    tdImc.textContent = "Altura Inválida";
+    palturaEValida = false;
+    pacientes[i].classList.add("paciente-invalido");
+  }
+
+  if (pesoEValido && palturaEValida){
+    tdImc.textContent = calculaImc(peso,altura);
+  }
+}
+
+
+function calculaImc(peso,altura){
+  var imc = 0;
+  imc = peso / (altura * altura);
+  return imc.toFixed(2);
+}
