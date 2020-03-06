36a6ec2a087e806eaccd0d467c40cc73c22de770
Breno César Baiardi <brenocbo@gmail.com>
2020-03-06 07:14:39 -0300

    adicionando primeira animação nos eventos

--- js/remover-paciente.js
+++ js/remover-paciente.js
@@ -1,7 +1,12 @@
 var tabela = document.querySelector("table");
 
-tabela.addEventListener("dblclick", function(){
+tabela.addEventListener("dblclick", function(event){
+  event.target.parentNode.classList.add("fadeOut");
+
+  setTimeout(function(){
   event.target.parentNode.remove(); //atrelado ao evento
+},500);
+
 });
 
 
